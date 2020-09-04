b578bc212aa8d550283e877f286371cc832b198d
Francis Chibaye <francischibaye@gmail.com>
2020-09-04 06:48:14 +0200

    init

--- src/controllers/receipts.ts
+++ src/controllers/receipts.ts
@@ -1,11 +1,16 @@
+import axios from 'axios'
+
 import Receipt from '../services/receipt'
 import launch from '../helpers/launch'
 
 export default {
     get: async (req, res) => {
         try {
+            res.send('Crawler started.')
             const data = await launch(Receipt.get)
-            res.send({data})
+            const url = 'https://retail.etoll.co.zm/api/v1.0/receipts/populate'
+            await axios.post(url, data)
+            return null
         } catch (e) {
             console.error(e)
             res.status(500).send({error: 'Something went wrong, try again'})
